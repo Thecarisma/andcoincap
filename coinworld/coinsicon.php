689dbf79e7863870818203de79af27fe89bde5be
Azeez Adewale <azeezadewale98@gmail.com>
2017-11-14 20:21:22 +0100

    Update coinsicon.php

--- coinworld/coinsicon.php
+++ coinworld/coinsicon.php
@@ -1,6 +1,34 @@
-<?php
+[
+
+  {
+     "name" : "bitcoin",
+     "url": "bitcoin.png"
+  }
+
+  {
+     "name" : "ethereum",
+     "url": "bitcoin.png"
+  }
+
+  {
+     "name" : "bitcoin_cash",
+     "url": "bitcoin.png"
+  }
+
+  {
+     "name" : "ripple",
+     "url": "bitcoin.png"
+  }
+
+  {
+     "name" : "litecoin",
+     "url": "bitcoin.png"
+  }
+
+  {
+     "name" : "dash",
+     "url": "bitcoin.png"
+  }
 
 
-
-
-/>
+]
