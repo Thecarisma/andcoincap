eb5132076a70cdd75b22a91b392027d85a3a86e7
Azeez Adewale <azeezadewale98@gmail.com>
2017-11-14 20:31:39 +0100

    Update coinsicon.php

--- coinworld/coinsicon.php
+++ coinworld/coinsicon.php
@@ -2,32 +2,32 @@
 
   {
      "name" : "bitcoin",
-     "url": "bitcoin.png"
+     "url": "http://bitcoin.org/img/icons/opengraph.png"
   }
 
   {
      "name" : "ethereum",
-     "url": "bitcoin.png"
+     "url": "http://files.coinmarketcap.com.s3-website-us-east-1.amazonaws.com/static/img/coins/200x200/ethereum.png"
   }
 
   {
      "name" : "bitcoin_cash",
-     "url": "bitcoin.png"
+     "url": "https://www.bitcoincash.org/img/bitcoincash-white.png"
   }
 
   {
      "name" : "ripple",
-     "url": "bitcoin.png"
+     "url": "http://files.coinmarketcap.com.s3-website-us-east-1.amazonaws.com/static/img/coins/200x200/ripple.png"
   }
 
   {
      "name" : "litecoin",
-     "url": "bitcoin.png"
+     "url": "http://files.coinmarketcap.com.s3-website-us-east-1.amazonaws.com/static/img/coins/200x200/litecoin.png"
   }
 
   {
      "name" : "dash",
-     "url": "bitcoin.png"
+     "url": "https://bittrexblobstorage.blob.core.windows.net/public/49993d38-d344-4197-b449-c50c3cc13d47.png"
   }
 
 
